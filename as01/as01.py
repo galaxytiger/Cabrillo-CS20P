2adfd13ce964fb7582d90c7d9d1fa35591e8d4b4
altorresmoran (Anthony L. Torres-Moran) <altorresmoran@jeff.cis.cabrillo.edu>
2023-02-07 11:49:51 -0800

    fixing some formatting and adding doc string

--- as01/as01.py
+++ as01/as01.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
-import sys
+'''
+The GC-content of a DNA string is given by the percentage of symbols
+in the string that are 'C' or 'G'.
+For example, the GC-content of "AGCTATAG" is 37.5%
+Sample Dataset
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT
+Sample Output
+Rosalind_0808
+60.919540
+'''
+
+__author__ = 'Anthony Torres for CS 12P, altorresmoran@jeff.cis.cabrillo.edu'
+
 
 def valid_dna(dna: str) -> str:
-  return ''.join(filter(lambda c: c in 'ACGT', dna.upper().strip()))
+  return ''.join(filter(lambda c: c in 'ACGT', dna.upper()))
 
 
 def gc_units(string):
   return ''.join(filter(lambda c: c in 'CG', string))
+
 
 def gc_content(string):
   if len(valid_dna(string)) == 0:
@@ -14,10 +29,12 @@ def gc_content(string):
   else:
     return (len(gc_units(string)) / len(valid_dna(string))) * 100
 
+
 if __name__ == '__main__':
   import sys
-  dna = []
+  dna_input = []
   for line in sys.stdin:
-    dna.append(valid_dna(line))
+    dna_input.append(valid_dna(line))
     pass
-print(gc_content(''.join(dna)))
+  print(''.join(dna_input))
+  print(gc_content(''.join(dna_input)))
