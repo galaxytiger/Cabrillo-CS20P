6224bc19c76a07162f5b71f7cd173b8e00dbf5aa
altorresmoran (Anthony L. Torres-Moran) <altorresmoran@jeff.cis.cabrillo.edu>
2023-02-07 11:34:38 -0800

    testing as01 gc content

--- as01/as01.py
+++ as01/as01.py
@@ -1,47 +1,23 @@
 #!/usr/bin/env python3
-
 import sys
+
 def valid_dna(dna: str) -> str:
   return ''.join(filter(lambda c: c in 'ACGT', dna.upper().strip()))
 
+
+def gc_units(string):
+  return ''.join(filter(lambda c: c in 'CG', string))
+
+def gc_content(string):
+  if len(valid_dna(string)) == 0:
+    return len(gc_units(string))
+  else:
+    return (len(gc_units(string)) / len(valid_dna(string))) * 100
+
 if __name__ == '__main__':
-  dna_len = []
-  g_total = []
-  c_total = []
+  import sys
   dna = []
   for line in sys.stdin:
     dna.append(valid_dna(line))
     pass
-  print(len(dna))
-  # gc_sum = (sum(g_total)) + (sum(c_total))
-  # dna_sum = sum(dna_len)
-
-  # # print(gc_sum, dna_sum)
-  # if dna_sum == 0:
-  #   print(gc_sum)
-  # else:
-  #   print((gc_sum / dna_sum) * 100)
-  # result = ""
-  #   for char in dna:
-  #       if char in "ACGTacgt":
-  #           result += char
-  #   return result
-# valid = set("ACGT")
-# dna_len = []
-# g_total = []
-# c_total = []
-# for line in sys.stdin:
-#   dna = line.upper().strip()
-
-#   g_total.append(dna.count('G'))
-#   c_total.append(dna.count('C'))
-#   dna_len.append(len(dna))
-#   pass
-# gc_sum = (sum(g_total)) + (sum(c_total))
-# dna_sum = sum(dna_len)
-
-# # print(gc_sum, dna_len)
-# if dna_sum == 0:
-#   print(gc_sum)
-# else:
-#   print((gc_sum / dna_sum) * 100)
+print(gc_content(''.join(dna)))
