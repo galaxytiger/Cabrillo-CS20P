cca67361cf204669714f3d61c79513551cdfd744
altorresmoran (Anthony L. Torres-Moran) <altorresmoran@jeff.cis.cabrillo.edu>
2023-02-07 09:26:07 -0800

    testing

--- as01/as01.py
+++ as01/as01.py
@@ -8,14 +8,11 @@ if __name__ == '__main__':
   dna_len = []
   g_total = []
   c_total = []
+  dna = []
   for line in sys.stdin:
-    dna = valid_dna(line)
-    g_total.append(dna.count('G'))
-    c_total.append(dna.count('C'))
-    dna_len.append(len(dna))
-    # print(dna)
+    dna.append(valid_dna(line))
     pass
-    print(dna_len)
+  print(dna)
   # gc_sum = (sum(g_total)) + (sum(c_total))
   # dna_sum = sum(dna_len)
 
