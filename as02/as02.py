c6aba596a16f9cd904ea0c5cbf2acb07e02851a6
Anthony Torres <anthony.torresmoran@gmail.com>
2023-02-14 20:12:30 -0800

    checking house of something text

--- as02/as02.py
+++ as02/as02.py
@@ -77,9 +77,24 @@ def automated_readability_index(text: str):
     return ari
 
 
-line = sys.stdin.read()
-if sentence_count(line) == 0:
-  print('0.000')
-else:
-  # ari
-  print(automated_readability_index(line))
+# line = sys.stdin.read()
+# if sentence_count(line) == 0:
+#   print('0.000')
+# else:
+#   # ari
+#   print(automated_readability_index(line))
+
+if __name__ == '__main__':
+  import sys
+  line = sys.stdin.read()
+  if sentence_count(line) == 0:
+    print(
+      f'0.000\t({character_count(line)} characters '
+      f'{word_count(line)} words {sentence_count(line)} sentences)'
+    )
+  else:
+    # ari
+    print(
+      f'{automated_readability_index(line):.3f}\t({character_count(line)} characters '
+      f'{word_count(line)} words {sentence_count(line)} sentences)'
+    )
