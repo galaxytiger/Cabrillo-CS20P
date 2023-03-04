e47ac00b5fc15906b47bdd6ba4c2810655f55187
Anthony Torres <anthony.torresmoran@gmail.com>
2023-03-03 22:13:45 -0800

    working on tokenize words function

--- as04/scrabble.py
+++ as04/scrabble.py
@@ -42,11 +42,12 @@ def tokenize_words(file: TextIOBase) -> Iterator[str]:
   >>> pprint(list(tokenize_words(open('/srv/datasets/phonewords-e.161.txt'))))
   ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
   """
-  file_contents = file.read()
+
   pattern = r'[A-Z]+'
-  words = re.findall(pattern, file_contents)
-  for word in words:
-    yield word.upper()
+  for line in file:
+    words = re.findall(pattern, line)
+    for word in words:
+      yield word.upper()
 
  
  
