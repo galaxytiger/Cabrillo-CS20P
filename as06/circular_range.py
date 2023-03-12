4ad25e43626240b213d7fa55b70616369a962960
Anthony Torres <anthony.torresmoran@gmail.com>
2023-03-11 17:08:45 -0800

    working on circle_actor

---
+++ as06/circular_range.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" Module for the CircularRange class. """
+
+__author__ = 'Anthony Torres for CS 20P, altorresmoran@jeff.cis.cabrillo.edu'
+
+
+class CircularRange:
+
+
+  def __init__(self, start, stop, step=1):
+    """Constructs a CircularRange with the given start/stop/step values."""
+    pass  # TODO
