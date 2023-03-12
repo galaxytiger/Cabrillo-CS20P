d9aa98a36c61b98fe5e0054fe291a6b2c9217df9
Anthony Torres <anthony.torresmoran@gmail.com>
2023-03-11 16:54:14 -0800

    working on circle_actor

---
+++ as05/test_circle_actor.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestCircleActor(TestCase):
+  def test_collide(self):
+    self.fail()
+
+  def test_position(self):
+    self.fail()
+
+  def test_radius(self):
+    self.fail()
+
+  def test_step(self):
+    self.fail()
+
+  def test_velocity(self):
+    self.fail()
