d6f64ff66834fa60ec09c2925f34e8c93abbef8f
Anthony Torres <anthony.torresmoran@gmail.com>
2023-03-28 18:17:53 -0700

    circular range

--- as06/circular_range.py
+++ as06/circular_range.py
@@ -14,9 +14,26 @@ class CircularRange(collections.abc.Sequence):
     self.start = start
     self.stop = stop
     self.step = step
+    self.range_length = (stop - start + step - 1) // step
 
   def __getitem__(self, index):
-    pass  # TODO
+    if isinstance(index, slice):
+      start, stop, step = index.indices(len(self))
+      new_start = self.start + self.start * start
+      new_stop = self.start + self.step * stop
+      if step < 0:
+        new_start, new_stop = new_stop + self.step, new_start + self.step
+      return CircularRange(new_start, new_stop, self.step * step)
+    else:
+      if index < 0:
+        index = -index
+      index = index % self.range_length
+      return self.start + index * self.step
 
   def __len__(self):
-    pass  # TODO
+    return self.range_length
+
+  def __contains__(self, item):
+    if item < self.start or item >= self.stop:
+      return False
+    return (item - self.start) % self.step == 0
