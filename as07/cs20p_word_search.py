7817f7d806deb689190447555f8e00674ecec18e
Anthony Torres <anthony.torresmoran@gmail.com>
2023-03-28 19:58:12 -0700

    working on word search

--- as07/cs20p_word_search.py
+++ as07/cs20p_word_search.py
@@ -3,3 +3,13 @@
 """ word search to re-traumatize me from my time in school """
 
 __author__ = 'Anthony Torres for CS 20P, altorresmoran@jeff.cis.cabrillo.edu'
+
+import sys
+import os
+import argparse
+
+class WordSearch:
+  def __init__(self):
+    self.dir = [[-1, 0], [1, 0], [1, 1],
+                    [1, -1], [-1, -1], [-1, 1],
+                    [0, 1], [0, -1]]
