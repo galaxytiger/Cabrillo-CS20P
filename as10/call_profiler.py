52b324445ecc2c761868b145fe0ec63369e3f71b
Anthony Torres <anthony.torresmoran@gmail.com>
2023-05-08 19:50:30 -0700

    working on as10

--- as10/call_profiler.py
+++ as10/call_profiler.py
@@ -9,6 +9,8 @@ __author__ = 'Anthony Torres for CS 20P, altorresmoran@jeff.cis.cabrillo.edu'
 
 import time
 
+_profiling_data = {}
+
 
 def profile(function):
   """
@@ -16,7 +18,21 @@ def profile(function):
   be reported by call_count() and cumulative_time(), respectively. Execution time is measured by
   calling time.perf_counter() before and after a call to the decorated function.
   """
-  pass  # TODO
+  def wrapper(*args, **kwargs):
+    start_time = time.perf_counter()
+    result = function(*args, **kwargs)
+    elapsed_time = time.perf_counter() - start_time
+
+    if function in _profiling_data:
+      _profiling_data[function]['count'] += 1
+      _profiling_data[function]['cumulative_time'] += elapsed_time
+    else:
+      _profiling_data[function] = {
+        'count': 1,
+        'cumulative_time': elapsed_time
+      }
+    return result
+  return wrapper
 
 
 def call_count(function):
@@ -24,7 +40,7 @@ def call_count(function):
   Returns the number of times a given function has been called during this interpreter session,
   assuming the function has been decorated by profile().
   """
-  pass  # TODO
+  return _profiling_data.get(function, {}).get('count', 0)
 
 
 def call_counts():
@@ -32,7 +48,7 @@ def call_counts():
   Returns a dictionary mapping functions decorated by profile() to the number of times they have
   been called during this interpreter session.
   """
-  pass  # TODO
+  return {func: data['count'] for func, data in _profiling_data.items()}
 
 
 def cumulative_time(function):
@@ -40,7 +56,7 @@ def cumulative_time(function):
   Returns the cumulative amount of time (in seconds) that have been spent executing calls to a given
   function during this interpreter session, assuming the function has been decorated by profile().
   """
-  pass  # TODO
+  return _profiling_data.get(function, {}).get('cumulative_time', 0.0)
 
 
 def cumulative_times():
@@ -48,4 +64,4 @@ def cumulative_times():
   Returns a dictionary mapping functions decorated by profile() to the cumulative amount of time (in
   seconds) that have been spent executing calls to a given function during this interpreter session.
   """
-  pass  # TODO
+  return {func: data['cumulative_time'] for func, data in _profiling_data.items()}
