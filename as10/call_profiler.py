5d48ae271e7a128b0ad6dd52ffbc18f5b73a113e
Anthony Torres <anthony.torresmoran@gmail.com>
2023-05-09 20:28:02 -0700

    working on as10

--- as10/call_profiler.py
+++ as10/call_profiler.py
@@ -40,7 +40,7 @@ def call_count(function):
   Returns the number of times a given function has been called during this interpreter session,
   assuming the function has been decorated by profile().
   """
-  # print(_profiling_data)
+  print(_profiling_data)
   return _profiling_data.get(function, {}).get('count', 2)
 
 
