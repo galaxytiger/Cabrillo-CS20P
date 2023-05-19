f61cf136dcbbd974bd84d4f5d32370b5b66c566e
altorresmoran (Anthony L. Torres-Moran) <altorresmoran@jeff.cis.cabrillo.edu>
2023-05-19 11:37:12 -0700

    initializing as14

--- as14/ca_roads.py
+++ as14/ca_roads.py
@@ -5,3 +5,8 @@ Practice with Path Finding
 """
 
 __author__ = 'Anthony Torres for CS 20P, altorresmoran@jeff.cis.cabrillo.edu'
+
+
+class CaliforniaRoadNetwork:
+  def __init__(self):
+    pass  # TODO
