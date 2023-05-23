124da57dc0e8a14600c7d25b017a7ba0af19be4c
Anthony Torres <anthony.torresmoran@gmail.com>
2023-05-22 22:32:23 -0700

    working on as12

--- as12/hash_set.py
+++ as12/hash_set.py
@@ -187,7 +187,8 @@ class HashSet:
     [None, None, None, None, None, None, None, None]
     """
     idx = self._find_key(key)
-    if self._table[idx] == key:
+    # if self._table[idx] == key:
+    if self._table[idx] is not None and self._table[idx] != self._DELETED:
       self._table[idx] = self._DELETED
       self._num_keys -= 1
       self._keys.remove(key)
